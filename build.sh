a748fe4da4459174a1623e6b4b15a3eba6a3b2a1
Gregory <neogen570@gmail.com>
2019-08-10 15:56:14 +0300

    add otool binary

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ OUT_DIR="$PWD/out"
 RELEASE_BLD_DIR="$PWD/bld"
 DEBUG_BLD_DIR="$PWD/debug_bld"
 
-TARGET="ft_nm"
+FT_NM="ft_nm"
+FT_OTOOL="ft_otool"
 
-meson $RELEASE_BLD_DIR --prefix=$OUT_DIR --libdir=$OUT_DIR --includedir=$OUT_DIR --buildtype=release
+meson $RELEASE_BLD_DIR --prefix=$OUT_DIR --libdir=$OUT_DIR --includedir=$OUT_DIR --bindir=$OUT_DIR --buildtype=release
 meson $DEBUG_BLD_DIR --buildtype=debug
 
 ninja -C $DEBUG_BLD_DIR
 ninja -C $RELEASE_BLD_DIR
 ninja -C $RELEASE_BLD_DIR install
 
-ln -sf "$OUT_DIR/bin/$TARGET" "$PWD/$TARGET"
+ln -sf "$OUT_DIR//$FT_NM" "$PWD/$FT_NM"
+ln -sf "$OUT_DIR/$FT_OTOOL" "$PWD/$FT_OTOOL"
